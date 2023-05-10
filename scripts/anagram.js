ed4d25344ced8d543e5e5721e0fb384b54569046
ismaelxyz <ismaelbeli.com@gmail.com>
2023-05-10 13:08:39 -0400

    add comments

--- scripts/anagram.js
+++ scripts/anagram.js
@@ -1,5 +1,17 @@
 const TD_QUERY = ".custom-table > tbody > tr > td";
+/*
+element.style {
+    transform: translate(10px, 0px);
+}
+*/
 
+
+
+/*
+  A function that is useful for creating a list with numbers in a range
+  starting at 'start', ending at 'stop', and 'step' controls the increment
+  between values in the range
+*/
 function arrayRange(start, stop, step) {
     return Array.from(
       { length: (stop - start) / step + 1 },
@@ -7,24 +19,27 @@ function arrayRange(start, stop, step) {
     );
 }
 
-
+/*
+  Makes all the letters of the text are together without spaces and in lowercase
+  to make it more difficult to recognize
+*/
 function dirtyText(text) {
   return text.toLocaleLowerCase().replaceAll(' ', '');
 }
+
 /*
-element.style {
-    transform: translate(10px, 0px);
-}
+  It is executed when the user presses 'Enter' and the program is focused on
+  the main input
 */
-
 function enterPress(event) {
-
   if (event.key == 'Enter')
     document.querySelector('#aceept-btn').click();
   
 }
 
-
+/*
+  This function creates an anagram based not on a word as such, but on a text
+*/
 function disarrangeTextCharacters(text) {
     text = dirtyText(text);
     let newText = text.split('');
@@ -40,51 +55,17 @@ function disarrangeTextCharacters(text) {
     return newText.join('')
 }
 
-
-function moveTo(event) {
-  console.log("Event: ", event);
-}
-
-let EditWord = {
-  'state': null,
-
-  'prepareElement': function(htmlElement) {
-    htmlElement.ontouchstart = EditWord.onEventStart;
-    htmlElement.ontouchend = EditWord.onEventEnd;
-    htmlElement.onmousedown = EditWord.onEventStart;
-    htmlElement.onmouseup = EditWord.onEventEnd;
-  },
-
-  'onEventStart': function(event) {
-    this.state = {
-      isStart: true,
-      htmlElement: event.srcElement,
-      x: event.x,
-      y: event.y,
-    };
-    // console.log("Ontouchstart: ", event.srcElement);
-  },
-
-  'onEventEnd': function(event) {
-    if (!this.state) return;
-
-    let location = {
-      x: this.state.x - event.x,
-      y: this.state.y - event.y
-    }
-
-    if (location.x < 0)
-      AnagramView.remove(this.state.htmlElement.parentNode);
-
-    this.state = null;
-  },
-};
-
+/*
+  Here all the texts that the user enters into the program are stored together
+  with their respective anagrams so that they can be manipulated in a simple way
+*/
 let Anagram = {
+  // A record of texts with their anagrams
   'texts': {},
+
   /*
     Insert or update a text into texts list, ruturns true if it is inserted for
-    firts time.
+    firts time
   */
   'addText': function(sourceText) {
     let undefinedSourceText = Anagram.texts[sourceText] === undefined;
@@ -93,23 +74,30 @@ let Anagram = {
     return undefinedSourceText;
   },
 
-  'wordIndex': function(word) {
-    return Object.keys(this.texts).indexOf(word);
+  // Answer the question Where is the text?
+  'textIndex': function(text) {
+    return Object.keys(this.texts).indexOf(text);
   },
 
-  'removeText': function(word) {
-    delete this.texts[word];
+  // Deletes a text and its anagram from the record
+  'removeText': function(text) {
+    delete this.texts[text];
   }
 };
 
+
+// The dynamics that make this program work
 let AnagramView = {
+  'selected': null,
+
   /*
     Each anagram is represented graphically. This function helps find the
-    graphic representation of an anagram according to its .index
+    graphic representation of an anagram according to its 'index'
   */
   'associatedIndex': function(index) {
     return 2 * index + 1;
   },
+
   /*
     Add or update the graphic representation of an anagram.
   */
@@ -126,37 +114,86 @@ let AnagramView = {
       anagramwordTd.textContent = Anagram.texts[sourceText];
 
       let trow = document.createElement("tr");
-
-      EditWord.prepareElement(trow);
-
-      // trow.onclick = moveTo;
+      trow.onclick = (event) => {
+        AnagramView.openEditMenu(event.srcElement.parentNode);
+      };
       column.appendChild(trow);
 
 
       trow.appendChild(wordTd);
       trow.appendChild(anagramwordTd);
     } else {
-      let index = this.associatedIndex(Anagram.wordIndex(sourceText));
+      let index = this.associatedIndex(Anagram.textIndex(sourceText));
       let anagramwordTd = document.querySelectorAll(TD_QUERY)[index];
       anagramwordTd.textContent = Anagram.texts[sourceText];
     }
 
   },
+  
+  'clearEditText': function() {
+      let anagramEdit = document.querySelector(".edit .anagram-edit");
+      let wordEdit = document.querySelector(".edit .word-edit");
+
+      if (this.selected) {
+        this.selected.childNodes[0].textContent = anagramEdit.value;
+        this.selected.childNodes[1].textContent = wordEdit.value;
+      }
+
+      anagramEdit.value = "";
+      wordEdit.value = "";
+
+  },
+
+  'accept': function() {
+      if (this.selected) {
+        this.clearEditText();
+        this.selected = null;
+        this.closeEditMenu();
+      }
+  },
+
   /*
     Remove the graphic representation of an anagram.
   */
-  'remove': function(parentNode) {
+  'remove': function() {
+      if (this.selected) {
+        Anagram.removeText(
+          this.selected.firstElementChild.textContent
+        );
+        this.selected.remove();
+
+        this.selected = null;
+        this.clearEditText();
+        this.closeEditMenu();
+      }
+  },
+
+  /*
+    Open the editing menu so that an anagram can be modified
+  */
+  'openEditMenu': function(parentNode) {
     let trs = document.querySelectorAll(".custom-table > tbody > tr");
 
-    for (let index = 0; index < trs.length; index++) {
+    let anagramEdit = document.querySelector(".edit .anagram-edit");
+    let wordEdit = document.querySelector(".edit .word-edit");
 
-      if (trs[index] == parentNode) {
-        Anagram.removeText(trs[index].firstElementChild.textContent);
-        parentNode.remove();
-        break;
-      }
+    anagramEdit.value = parentNode.childNodes[0].textContent;
+    wordEdit.value = parentNode.childNodes[1].textContent;
 
-    }
+    this.selected = parentNode;
+    
+    let edit = document.querySelector(".edit")
+    edit.style.display = "block";
+  },
+
+
+  /*
+    Close the editing menu
+  */
+  'closeEditMenu': function() {
+      let edit = document.querySelector(".edit")
+      edit.style.display = "none";
   }
+
 }
 
